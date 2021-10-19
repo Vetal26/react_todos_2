106362d60530749a0da61352fadbbff13a41bd41
Vitaly Borisov <vitaliy.borisov@intspirit.com>
2021-10-19 18:49:51 +0300

    added components, pages

---
+++ src/components/Notes.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Notes = ({ notes }) => {
+  return (
+    <ul className="list-group">
+      {notes.map((note) => (
+        <li className="list-group-item note" key={note.id}>
+          {note.title}
+          <button type="button" className="btn btn-danger btn-sm">
+            &times;
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div className="p-5 mb-4 bg-light rounded-3">
+      <div className="container">
+        <h1 className="display-4">REACT APP</h1>
+        <p className="lead">
+          Version <strong>0.1.0</strong>
+        </p>
+      </div>
+    </div>
+  );
+};
