6b9934be620ce587af8c16ed5e7bd962855fca5e
Vitaly Borisov <vitaliy.borisov@intspirit.com>
2021-10-20 15:59:18 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Alert } from "./components/Alert";
 import { Navbar } from "./components/Navbar";
+import { AlertState } from "./context/alert/AlertState";
+import { FirebaseState } from "./context/firebase/FirebaseState";
 import { About } from "./pages/About";
 import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <div className="container pt-4">
-        <Switch>
-          <Route path={"/"} exact component={Home} />
-          <Route path={"/about"} component={About} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <FirebaseState>
+      <AlertState>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container pt-4">
+            <Alert />
+            <Switch>
+              <Route path={"/"} exact component={Home} />
+              <Route path={"/about"} component={About} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </AlertState>
+    </FirebaseState>
   );
 }
 
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,16 +1,24 @@
 import React from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
-export const Notes = ({ notes }) => {
-  return (
-    <ul className="list-group">
-      {notes.map((note) => (
-        <li className="list-group-item note" key={note.id}>
-          {note.title}
-          <button type="button" className="btn btn-danger btn-sm">
+export const Notes = ({ notes, onRemove }) => (
+  <TransitionGroup component="ul" className="list-group">
+    {notes.map((note) => (
+      <CSSTransition key={note.id} classNames={"note"} timeout={800}>
+        <li className="list-group-item note">
+          <div>
+            <strong>{note.title}</strong>
+            <small>{note.date}</small>
+          </div>
+          <button
+            type="button"
+            className="btn btn-danger btn-sm"
+            onClick={() => onRemove(note.id)}
+          >
             &times;
           </button>
         </li>
-      ))}
-    </ul>
-  );
-};
+      </CSSTransition>
+    ))}
+  </TransitionGroup>
+);
